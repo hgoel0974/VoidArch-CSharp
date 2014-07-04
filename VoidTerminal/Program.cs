68476c31bee08f650c91f8c117bad59e51ebd301
hgoel0974 <hgoel0974@gmail.com>
2014-07-04 19:11:07 -0400

    Added Void terminal

---
+++ VoidTerminal/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VoidArch;
+
+namespace VoidTerminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.CursorVisible = true;
+
+            Console.WriteLine("Void Terminal v" + typeof(Program).Assembly.GetName().Version);
+            Console.WriteLine("Void Platform v" + typeof(Assembler).Assembly.GetName().Version);
+
+            while (true)
+            {
+                Console.Write("\nvoid>");
+                string cmd = Console.ReadLine().Trim().ToLowerInvariant();
+
+                string[] @params = cmd.Split(' ');
+
+                if (@params[0] == "ls")
+                {
+                    #region LS
+                    var data = Directory.EnumerateFileSystemEntries((@params.Length > 1) ? @params[1] : Environment.CurrentDirectory);
+
+                    foreach (string entry in data)
+                    {
+                        if (Directory.Exists(entry))
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                        }
+
+                        Console.WriteLine("\t" + Path.GetFileName(entry));
+
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                    }
+                    #endregion
+                }
+                else if (@params[0] == "cd")
+                {
+                    #region CD
+                    Environment.CurrentDirectory = @params[1];
+                    #endregion
+                }
+                else if (@params[0] == "wdir")
+                {
+                    #region Wdir
+                    Console.WriteLine(Environment.CurrentDirectory);
+                    #endregion
+                }
+                else if (@params[0] == "vasm")
+                {
+                    #region Void Assembler
+
+                    #endregion
+                }
+                else if (@params[0] == "vdasm")
+                {
+                    #region Void Disassembler
+
+                    #endregion
+                }
+                else if (@params[0] == "void")
+                {
+                    #region Void VM
+
+                    #endregion
+                }
+                else if (@params[0] == "mekax")
+                {
+                    #region MekaX
+
+                    #endregion
+                }
+                else if (@params[0] == "help" || @params[0] == "?")
+                {
+                    #region Help
+
+                    Console.WriteLine("\t{0}\t:\t{1}", "ls", "List the directory contents");
+                    Console.WriteLine("\t{0}\t:\t{1}", "cd", "Change the current working directory");
+                    Console.WriteLine("\t{0}\t:\t{1}", "wdir", "Show the current working directory");
+                    Console.WriteLine("\t{0}\t:\t{1}", "vasm", "Invoke the void assembler");
+                    Console.WriteLine("\t{0}\t:\t{1}", "vdasm", "Invoke the void disassembler");
+                    Console.WriteLine("\t{0}\t:\t{1}", "void", "Invoke the void VM");
+                    Console.WriteLine("\t{0}\t:\t{1}", "mekax", "Invoke the MekaX compiler");
+
+                    #endregion
+                }
+
+
+            }
+
+
+        }
+    }
+}
--- VoidTerminal/Properties/AssemblyInfo.cs
+++ VoidTerminal/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VoidArch")]
-[assembly: AssemblyDescription("C# Implementation of the void platform")]
+[assembly: AssemblyTitle("VoidTerminal")]
+[assembly: AssemblyDescription("Terminal for the void platform - interface to the rest of the system")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VoidArch")]
+[assembly: AssemblyProduct("VoidTerminal")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("87515915-5c00-4048-84e8-57fbb2fcc097")]
+[assembly: Guid("4df72742-75e5-4bf2-8d6c-12aeb8041c52")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.*")]
-[assembly: AssemblyFileVersion("0.0.*")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
